dafb1522dddaf45af5ceb08aba3cae16d1dafb8d
aditya.vemulapalli <aditya.vemulapalli@dremio.com>
2022-06-27 17:57:45 +0530

    export Backend Java

---
+++ export/src/main/java/exportBackend.java
@@ -0,0 +1,62 @@
+import org.projectnessie.versioned.Hash;
+import org.projectnessie.versioned.persist.adapter.*;
+
+import java.util.stream.Stream;
+
+public class exportBackend {
+
+    public Stream<CommitLogEntry> getCommitLogTable(DatabaseAdapter dbAdapter)
+    {
+        Stream<CommitLogEntry> StreamOfAllCommitLogEntries = dbAdapter.scanAllCommitLogEntries();
+
+        return  StreamOfAllCommitLogEntries;
+    }
+
+    // I think should be split into tx and non tx as
+    // getting initialHash Logic might be difficult
+    //like adding abstract method to get initialHash
+    //which can be implemented in exportTx and exportNonTx
+    public Stream <RefLog> getRefLogTable(DatabaseAdapter dbAdapter)
+    {
+        Hash initialHash;
+
+
+        Stream <RefLog> StreamOfAllRefLogEntries;
+
+        StreamOfAllRefLogEntries = dbAdapter.refLog(initialHash);
+
+        return  StreamOfAllRefLogEntries;
+    }
+
+    public RepoDescription getRepoDesc(DatabaseAdapter dbAdapter)
+    {
+        RepoDescription repoDescTable = dbAdapter.fetchRepositoryDescription();
+
+        return repoDescTable;
+    }
+
+    //Total Logic can be here
+    public void exportCommitLogTableIntoFile(Stream<CommitLogEntry> commitLogTable )
+    {
+
+    }
+
+    //Total Logic can be here
+    public void exportRefLogTableIntoFile(Stream <RefLog> refLogTable )
+    {
+
+    }
+
+    //Total Logic can be here
+    public void exportRepoDescIntoFile(RepoDescription repoDescTable)
+    {
+
+    }
+
+    //Total Logic can be here
+    public void exportKeyListsTableIntoFile( Stream<KeyListEntity> keysListsTable )
+    {
+
+    }
+
+}
